c36c719023aaa55578b6756b68e7b8ba2acd7c8c
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-17 11:37:15 +0100

    Added info panel component

---
+++ src/components/InfoPanel.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  message: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <label>Add an icon</label>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+div {
+  display: flex;
+  flex-direction: column;
+}
+</style>
