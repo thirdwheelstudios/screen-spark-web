86d621349990ad5d00e3ce3d69cd8810832a6189
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-17 11:37:47 +0100

    Screen Viewer wip

--- src/views/ScreenViewerView.vue
+++ src/views/ScreenViewerView.vue
@@ -1,1 +1,31 @@
-<template>Viewer</template>
+<script setup lang="ts">
+import { computed, onMounted } from 'vue'
+import { useSocketsStore } from '../store'
+import InfoPanel from '../components/InfoPanel.vue'
+
+const socketsStore = useSocketsStore()
+
+const isConnected = computed(() => socketsStore.isConnected)
+const connectionId = computed(() => socketsStore.id)
+
+onMounted(() => {
+  socketsStore.connect()
+})
+</script>
+
+<template>
+  <div v-if="isConnected">Connected</div>
+  <InfoPanel v-else message="Establishing connection, please wait..." />
+</template>
+
+<style scoped lang="scss">
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
