9212923a68ccd060a0efe22f83250ae7a913177a
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-17 14:44:51 +0100

    Finished initial 'connecting' dialog

--- src/views/ScreenViewerView.vue
+++ src/views/ScreenViewerView.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { computed, onMounted, ref, watch } from 'vue'
 import { useSocketsStore } from '../store'
-import CenteredInfoPanel from '../components/CenteredInfoCard.vue'
 import { getInterval } from '../composables'
+import IconInfoCard from '../components/IconInfoCard.vue'
 
 const socketsStore = useSocketsStore()
 const { intervalTrigger } = getInterval(10000)
@@ -10,6 +10,9 @@ const { intervalTrigger } = getInterval(10000)
 const message = ref('Connecting to Spark, please wait...')
 
 const isConnected = computed(() => socketsStore.isConnected)
+const isSlowToConnect = computed(
+  () => !isConnected.value && intervalTrigger.value
+)
 const connectionId = computed(() => socketsStore.id)
 
 onMounted(() => {
@@ -28,16 +31,50 @@ watch(
 </script>
 
 <template>
-  <div v-if="isConnected">Connected</div>
-  <CenteredInfoPanel v-else :message="message" icon="circle-notch" :spin="true"
-    ><p>{{ message }}</p></CenteredInfoPanel
-  >
+  <Transition>
+    <div v-if="isConnected">connected: {{ connectionId }}</div>
+    <div v-else class="status-container">
+      <IconInfoCard
+        header="Connecting to Spark"
+        :message="message"
+        icon="circle-notch"
+        :spin="true"
+        :shadow="true"
+        class="status-card"
+      >
+        <div v-if="isSlowToConnect">
+          <p class="m-3">
+            We are currently experiencing problems connecting to Spark
+          </p>
+          <p>Please bear with us whilst we keep trying...</p>
+        </div>
+        <p class="m-3" v-else>Connecting to Spark, please wait...</p>
+      </IconInfoCard>
+    </div>
+  </Transition>
 </template>
 
 <style scoped lang="scss">
+.status-container {
+  display: flex;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+
+  .status-card {
+    height: 210px;
+    width: 550px;
+  }
+}
+
 .v-enter-active,
 .v-leave-active {
-  transition: opacity 0.5s ease;
+  transition: opacity 1s ease;
+  transition-delay: 0.5s;
 }
 
 .v-enter-from,
