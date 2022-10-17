1755d5a1e42375db00d91937b767d40d8945b7a4
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-17 13:24:12 +0100

    Setup font awesome

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 import './style.css'
 import App from './App.vue'
 import router from './router'
 
+library.add(faCircleNotch)
+
 const pinia = createPinia()
 
-createApp(App).use(pinia).use(router).mount('#app')
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .use(pinia)
+  .use(router)
+  .mount('#app')
