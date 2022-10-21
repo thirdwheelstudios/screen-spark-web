2debf77283161f875f9213fabae006097a9d4b48
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-21 10:45:55 +0100

    Added prop

--- src/views/ScreenBroadcasterView.vue
+++ src/views/ScreenBroadcasterView.vue
@@ -1,1 +1,16 @@
-<template>Broadcaster</template>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useScreenStreaming } from '../composables'
+
+interface Props {
+  receiverId: string
+}
+
+const props = defineProps<Props>()
+
+const { socketsStore, webRTCStore } = useScreenStreaming(props.receiverId)
+
+onMounted(() => {})
+</script>
+
+<template>Broadcaster: {{ receiverId }}</template>
