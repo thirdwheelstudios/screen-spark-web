0bf05c343fe4de06c993f0586cb671ef68df9145
Phil Crossland <phil@thirdwheelstudios.com>
2022-10-21 10:47:04 +0100

    Unused import

--- src/views/ScreenViewerView.vue
+++ src/views/ScreenViewerView.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, watch } from 'vue'
+import { computed, watch } from 'vue'
 import { storeToRefs } from 'pinia'
 import { getInterval, useScreenStreaming } from '../composables'
 import IconInfoCard from '../components/IconInfoCard.vue'
