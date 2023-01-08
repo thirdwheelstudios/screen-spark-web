42c480832b8aaac2c613436aaec928796a511657
Phil Crossland <phil@thirdwheelstudios.com>
2023-01-08 21:53:15 +0000

    Created divider component

---
+++ src/components/ContentDivider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <hr class="mx-1" />
+    <slot></slot>
+    <hr class="mx-1" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+div {
+  display: flex;
+
+  hr {
+    flex-grow: 1;
+  }
+}
+</style>
