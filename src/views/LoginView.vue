ba8801ac612913ddb8418f8d1df6ba7fd89e4ef0
Phil Crossland <phil@thirdwheelstudios.com>
2023-01-08 21:53:25 +0000

    Login form wip

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,97 @@
 <script setup lang="ts">
-import { useNetlifyIdentity } from '../composables'
+import { ref } from 'vue'
+import ContentDivider from '../components/ContentDivider.vue';
 
-const { open, openLogin, openSignup } = useNetlifyIdentity()
+const email = ref<string>()
+
+const formSubmitted = () => {}
 </script>
 
 <template>
-    <button @click="open">Open</button>
-    <button @click="openLogin">Login</button>
-    <button @click="openSignup">Signup</button>
+  <div class="login-container">
+    <div>
+      <router-link to="/" class="brand m-3">
+        <img src="../assets/screen-spark-flame.svg" title="Screen Spark"
+      /></router-link>
+      <span>To continue, please sign in</span>
+    </div>
+    <div class="m-3">
+      <form action="post" @submit.prevent="formSubmitted">
+        <h3>Sign in</h3>
+        <div class="d-grid gap-2">
+          <button class="btn btn-primary" type="button">
+            Sign in with Google
+          </button>
+          <button class="btn btn-primary" type="button">
+            Sign in with Apple
+          </button>
+        </div>
+        <ContentDivider>or</ContentDivider>
+        <div class="mb-3">
+          <input
+            type="email"
+            id="email"
+            class="form-control"
+            v-model="email"
+            placeholder="Email"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+          />
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.login-container {
+  display: flex;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+
+  > div {
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    text-align: center;
+    justify-content: center;
+
+    .brand {
+      display: flex;
+      justify-content: center;
+    }
+
+    form {
+      text-align: left;
+      max-width: 400px;
+    }
+  }
+}
+
+@media only screen and (max-width: 575px) {
+  .login-container {
+    flex-direction: column;
+
+    div {
+      margin: 2rem auto;
+      flex-grow: 0;
+
+      .brand {
+        img {
+          max-width: 35%;
+        }
+      }
+    }
+  }
+}
+</style>
